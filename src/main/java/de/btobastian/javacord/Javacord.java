ae205dbf33d5f5f09d98ba8f544166604a07d23d
BtoBastian <bastianoppermann1997@gmail.com>
2016-03-13 13:12:20 +0100

    Incremented version number

--- src/main/java/de/btobastian/javacord/Javacord.java
+++ src/main/java/de/btobastian/javacord/Javacord.java
@@ -29,7 +29,7 @@ public class Javacord {
     /**
      * The current javacord version.
      */
-    public static final String VERSION = "2.0.8";
+    public static final String VERSION = "2.0.9";
 
     /**
      * The github url of javacord.
