55d701290f82aa8fac551226c06aad123f7c91f7
Bastian Oppermann <bastianoppermann1997@gmail.com>
2017-05-23 08:28:27 +0200

    Revert "Add support for editing a roles mentionable and managed values"

--- src/main/java/de/btobastian/javacord/entities/permissions/Role.java
+++ src/main/java/de/btobastian/javacord/entities/permissions/Role.java
@@ -128,7 +128,7 @@ public interface Role {
     /**
      * Updates the permissions of this role.
      * If you want to update other things like name, color, etc. use
-     * {@link #update(String, Color, boolean, Permissions, boolean, boolean)} or your previous updates may be overwritten.
+     * {@link #update(String, Color, boolean, Permissions)} or your previous updates may be overwritten.
      *
      * @param permissions The permissions to set. Roles don't have the {@link PermissionState#NONE} so every
      *                    permission with this state will be replaced with {@link PermissionState#DENIED}.
@@ -139,7 +139,7 @@ public interface Role {
     /**
      * Updates the name of this role.
      * If you want to update other things like permissions, color, etc. use
-     * {@link #update(String, Color, boolean, Permissions, boolean, boolean)} or your previous updates may be overwritten.
+     * {@link #update(String, Color, boolean, Permissions)} or your previous updates may be overwritten.
      *
      * @param name The name to set.
      * @return A future which tells us if the update was successful or not.
@@ -149,7 +149,7 @@ public interface Role {
     /**
      * Updates the color of this role.
      * If you want to update other things like permissions, name, etc. use
-     * {@link #update(String, Color, boolean, Permissions, boolean, boolean)} or your previous updates may be overwritten.
+     * {@link #update(String, Color, boolean, Permissions)} or your previous updates may be overwritten.
      *
      * @param color The color to set.
      * @return A future which tells us if the update was successful or not.
@@ -159,33 +159,13 @@ public interface Role {
     /**
      * Updates the hoist of this role.
      * If you want to update other things like permissions, color, etc. use
-     * {@link #update(String, Color, boolean, Permissions, boolean, boolean)} or your previous updates may be overwritten.
+     * {@link #update(String, Color, boolean, Permissions)} or your previous updates may be overwritten.
      *
      * @param hoist The hoist to set.
      * @return A future which tells us if the update was successful or not.
      */
     public Future<Void> updateHoist(boolean hoist);
 
-    /**
-     * Updates the managed of this role.
-     * If you want to update other things like permissions, color, etc. use
-     * {@link #update(String, Color, boolean, Permissions, boolean, boolean)} or your previous updates may be overwritten.
-     * 
-     * @param managed The managed to set.
-     * @return A future which tells us if the update was successful or not.
-     */
-    public Future<Void> updateManaged(boolean managed);
-    
-    /**
-     * Updates the mentionable of this role.
-     * If you want to update other things like permissions, color, etc. use
-     * {@link #update(String, Color, boolean, Permissions, boolean, boolean)} or your previous updates may be overwritten.
-     * 
-     * @param mentionable The mentionable to set.
-     * @return A future which tells us if the update was successful or not.
-     */
-    public Future<Void> updateMentionable(boolean mentionable);
-    
     /**
      * Updates the role.
      *
