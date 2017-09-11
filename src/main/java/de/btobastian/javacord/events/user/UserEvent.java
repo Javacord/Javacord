35786a386d604921380080508577979cbf0495af
BtoBastian <bastianoppermann1997@gmail.com>
2017-09-11 17:46:50 +0200

    Added UserEvent

---
+++ src/main/java/de/btobastian/javacord/events/user/UserEvent.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2017 Bastian Oppermann
+ * 
+ * This file is part of Javacord.
+ * 
+ * Javacord is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser general Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ * 
+ * Javacord is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+package de.btobastian.javacord.events.user;
+
+import de.btobastian.javacord.DiscordApi;
+import de.btobastian.javacord.entities.User;
+import de.btobastian.javacord.events.Event;
+
+/**
+ * A user event.
+ */
+public abstract class UserEvent extends Event {
+
+    /**
+     * The user of the event.
+     */
+    private final User user;
+
+    /**
+     * Creates a new user event.
+     *
+     * @param api The api instance of the event.
+     * @param user The user of the event.
+     */
+    public UserEvent(DiscordApi api, User user) {
+        super(api);
+        this.user = user;
+    }
+
+    /**
+     * Gets the user of the event.
+     *
+     * @return The user of the event.
+     */
+    public User getUser() {
+        return user;
+    }
+
+}
