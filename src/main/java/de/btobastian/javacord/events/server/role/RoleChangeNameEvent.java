34b6996b4dcfad9bb9a3c9ece3d7d6dd0841deb3
kenwiel <kenwiel@hotmail.de>
2017-09-11 18:39:13 +0200

    Added RoleChangeNameEvent

---
+++ src/main/java/de/btobastian/javacord/events/server/role/RoleChangeNameEvent.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2017 Bastian Oppermann
+ *
+ * This file is part of Javacord.
+ *
+ * Javacord is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser general Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Javacord is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+package de.btobastian.javacord.events.server.role;
+
+import de.btobastian.javacord.DiscordApi;
+import de.btobastian.javacord.entities.Server;
+import de.btobastian.javacord.entities.permissions.Role;
+
+/**
+ * A Role Change Name event.
+ */
+public class RoleChangeNameEvent extends RoleEvent {
+
+    /**
+     * The old name of the role.
+     */
+    private final String oldName;
+
+    /**
+     * Creates a new RoleChangeNameEvent
+     *
+     * @param api The api instance of the event.
+     * @param server The server of the event.
+     * @param role The role of the event.
+     * @param oldName The old name of the role.
+     */
+    public RoleChangeNameEvent(DiscordApi api, Server server, Role role, String oldName) {
+        super(api, server, role);
+        this.oldName = oldName;
+    }
+
+    /**
+     * Gets the old name of the role.
+     *
+     * @return The old name of the role.
+     */
+    public String getOldName() {
+        return oldName;
+    }
+
+    /**
+     * Gets the new name of the role.
+     *
+     * @return The new name of the role.
+     */
+    public String getNewName() {
+        // TODO: return getRole().getName();
+        return null;
+    }
+}
