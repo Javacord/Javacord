13e7cae5ede7aa27213fb0d8c08ee4fd66252ad6
kenwiel <kenwiel@hotmail.de>
2017-09-11 18:22:42 +0200

    Added abstract UserRoleEvent

---
+++ src/main/java/de/btobastian/javacord/events/server/role/UserRoleEvent.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2017 Bastian Oppermann
+ *
+ * This file is part of Javacord.
+ *
+ * Javacord is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser general Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Javacord is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+package de.btobastian.javacord.events.server.role;
+
+import de.btobastian.javacord.DiscordApi;
+import de.btobastian.javacord.entities.Server;
+import de.btobastian.javacord.entities.User;
+import de.btobastian.javacord.entities.permissions.Role;
+
+/**
+ * A User role event.
+ */
+public abstract class UserRoleEvent extends RoleEvent {
+
+    /**
+     * The user of the event.
+     */
+    private final User user;
+
+    /**
+     * Creates a new UserRoleEvent
+     *
+     * @param api The api instance of the event.
+     * @param server The server of the event.
+     * @param role The role of the event.
+     * @param user The user of the event.
+     */
+    public UserRoleEvent(DiscordApi api, Server server, Role role, User user) {
+        super(api, server, role);
+        this.user = user;
+    }
+
+    /**
+     * Gets the user of the event.
+     *
+     * @return The user of the event.
+     */
+    public User getuser() {
+        return user;
+    }
+}
