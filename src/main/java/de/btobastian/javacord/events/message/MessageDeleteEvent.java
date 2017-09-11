024d69847ec121d6a6f26ad875c88d2593b12f25
BtoBastian <bastianoppermann1997@gmail.com>
2017-09-11 17:44:50 +0200

    Added MessageDeleteEvent

---
+++ src/main/java/de/btobastian/javacord/events/message/MessageDeleteEvent.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2017 Bastian Oppermann
+ * 
+ * This file is part of Javacord.
+ * 
+ * Javacord is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser general Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ * 
+ * Javacord is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+package de.btobastian.javacord.events.message;
+
+import de.btobastian.javacord.DiscordApi;
+import de.btobastian.javacord.entities.channels.TextChannel;
+
+/**
+ * A message delete event.
+ */
+public class MessageDeleteEvent extends OptionalMessageEvent {
+
+    /**
+     * Creates a new message delete event.
+     *
+     * @param api The discord api instance.
+     * @param messageId The id of the message.
+     * @param channel The text channel in which the message was sent.
+     */
+    public MessageDeleteEvent(DiscordApi api, long messageId, TextChannel channel) {
+        super(api, messageId, channel);
+    }
+
+}
