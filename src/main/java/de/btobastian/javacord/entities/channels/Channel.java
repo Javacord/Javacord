94863f4a5601f245449dbae00c3e10f9c4a2d840
BtoBastian <bastianoppermann1997@gmail.com>
2017-09-12 18:39:58 +0200

    Added some helper methods to Channel and TextChannelEvent to easily convert them

--- src/main/java/de/btobastian/javacord/entities/channels/Channel.java
+++ src/main/java/de/btobastian/javacord/entities/channels/Channel.java
@@ -2,10 +2,95 @@ package de.btobastian.javacord.entities.channels;
 
 import de.btobastian.javacord.entities.DiscordEntity;
 
+import java.util.Optional;
+
 /**
  * The class represents a channel.
  */
 public interface Channel extends DiscordEntity {
 
+    /**
+     * Gets the channel as group channel.
+     *
+     * @return The channel as group channel.
+     */
+    default Optional<GroupChannel> asGroupChannel() {
+        if (this instanceof GroupChannel) {
+            return Optional.of((GroupChannel) this);
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Gets the channel as private channel.
+     *
+     * @return The channel as private channel.
+     */
+    default Optional<PrivateChannel> asPrivateChannel() {
+        if (this instanceof PrivateChannel) {
+            return Optional.of((PrivateChannel) this);
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Gets the channel as server channel.
+     *
+     * @return The channel as server channel.
+     */
+    default Optional<ServerChannel> asServerChannel() {
+        if (this instanceof ServerChannel) {
+            return Optional.of((ServerChannel) this);
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Gets the channel as server text channel.
+     *
+     * @return The channel as server text channel.
+     */
+    default Optional<ServerTextChannel> asServerTextChannel() {
+        if (this instanceof ServerTextChannel) {
+            return Optional.of((ServerTextChannel) this);
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Gets the channel as server voice channel.
+     *
+     * @return The channel as server voice channel.
+     */
+    default Optional<ServerVoiceChannel> asServerVoiceChannel() {
+        if (this instanceof ServerVoiceChannel) {
+            return Optional.of((ServerVoiceChannel) this);
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Gets the channel as text channel.
+     *
+     * @return The channel as text channel.
+     */
+    default Optional<TextChannel> asTextChannel() {
+        if (this instanceof GroupChannel) {
+            return Optional.of((TextChannel) this);
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Gets the channel as voice channel.
+     *
+     * @return The channel as voice channel.
+     */
+    default Optional<VoiceChannel> asVoiceChannel() {
+        if (this instanceof VoiceChannel) {
+            return Optional.of((VoiceChannel) this);
+        }
+        return Optional.empty();
+    }
 
 }
--- src/main/java/de/btobastian/javacord/events/user/TextChannelEvent.java
+++ src/main/java/de/btobastian/javacord/events/user/TextChannelEvent.java
@@ -2,7 +2,12 @@ package de.btobastian.javacord.events.user;
 
 import de.btobastian.javacord.DiscordApi;
 import de.btobastian.javacord.entities.User;
+import de.btobastian.javacord.entities.channels.GroupChannel;
+import de.btobastian.javacord.entities.channels.PrivateChannel;
+import de.btobastian.javacord.entities.channels.ServerTextChannel;
 import de.btobastian.javacord.entities.channels.TextChannel;
+
+import java.util.Optional;
 
 /**
  * A text channel event which effects a user.
@@ -33,5 +38,35 @@ public abstract class TextChannelEvent extends UserEvent {
      */
     public TextChannel getChannel() {
         return channel;
+    }
+
+    /**
+     * Gets the server text channel of the event.
+     * Not present, if the channel is not a server text channel.
+     *
+     * @return The server text channel of the event.
+     */
+    public Optional<ServerTextChannel> getServerTextChannel() {
+        return channel.asServerTextChannel();
+    }
+
+    /**
+     * Gets the private channel of the event.
+     * Not present, if the channel is not a pricvate channel.
+     *
+     * @return The pricate channel of the event.
+     */
+    public Optional<PrivateChannel> getPrivateChannel() {
+        return channel.asPrivateChannel();
+    }
+
+    /**
+     * Gets the group channel of the event.
+     * Not present, if the channel is not a group channel.
+     *
+     * @return The group channel of the event.
+     */
+    public Optional<GroupChannel> getGroupChannel() {
+        return channel.asGroupChannel();
     }
 }
