44864174d426f5e1fa68044af265072d49cb8dbe
BtoBastian <bastianoppermann1997@gmail.com>
2017-10-21 21:40:09 +0200

    Added GroupChannel#updateName(String)

--- src/main/java/de/btobastian/javacord/entities/channels/GroupChannel.java
+++ src/main/java/de/btobastian/javacord/entities/channels/GroupChannel.java
@@ -1,10 +1,15 @@
 package de.btobastian.javacord.entities.channels;
 
+import com.mashape.unirest.http.HttpMethod;
 import de.btobastian.javacord.entities.IconHolder;
 import de.btobastian.javacord.entities.User;
+import de.btobastian.javacord.utils.rest.RestEndpoint;
+import de.btobastian.javacord.utils.rest.RestRequest;
+import org.json.JSONObject;
 
 import java.util.Collection;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * This class represents a group channel. Group channels are not supported by bot accounts!
@@ -29,5 +34,18 @@ public interface GroupChannel extends TextChannel, VoiceChannel, IconHolder {
      * @return The name of the channel.
      */
     Optional<String> getName();
+
+    /**
+     * Updates the name of the channel.
+     *
+     * @param name The new name of the channel.
+     * @return A future to check if the update was successful.
+     */
+    default CompletableFuture<Void> updateName(String name) {
+        return new RestRequest<Void>(getApi(), HttpMethod.PATCH, RestEndpoint.CHANNEL)
+                .setUrlParameters(String.valueOf(getId()))
+                .setBody(new JSONObject().put("name", name == null ? JSONObject.NULL : name))
+                .execute(res -> null);
+    }
 
 }
