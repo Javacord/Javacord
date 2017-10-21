813076486a7379c8c5d78d1403e43a95adce5529
BtoBastian <bastianoppermann1997@gmail.com>
2017-10-21 21:47:26 +0200

    Added ServerTextChannel#updateNsfwFlag(boolean)

--- src/main/java/de/btobastian/javacord/entities/channels/ServerTextChannel.java
+++ src/main/java/de/btobastian/javacord/entities/channels/ServerTextChannel.java
@@ -1,11 +1,16 @@
 package de.btobastian.javacord.entities.channels;
 
+import com.mashape.unirest.http.HttpMethod;
 import de.btobastian.javacord.entities.Mentionable;
 import de.btobastian.javacord.entities.User;
 import de.btobastian.javacord.entities.permissions.PermissionType;
+import de.btobastian.javacord.utils.rest.RestEndpoint;
+import de.btobastian.javacord.utils.rest.RestRequest;
+import org.json.JSONObject;
 
 import java.util.Collection;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * This class represents a server text channel.
@@ -30,6 +35,19 @@ public interface ServerTextChannel extends ServerChannel, TextChannel, Mentionab
      * @return The category of the channel.
      */
     Optional<ChannelCategory> getCategory();
+
+    /**
+     * Updates the nsfw (not safe for work) flag of the channel.
+     *
+     * @param nsfw The new nsfw flag to set.
+     * @return A future to check if the update was successful.
+     */
+    default CompletableFuture<Void> updateNsfwFlag(boolean nsfw) {
+        return new RestRequest<Void>(getApi(), HttpMethod.PATCH, RestEndpoint.CHANNEL)
+                .setUrlParameters(String.valueOf(getId()))
+                .setBody(new JSONObject().put("nsfw", nsfw))
+                .execute(res -> null);
+    }
 
     /**
      * Checks if the given user can send messages in this channel.
