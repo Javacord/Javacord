f144147cbf6a1c694df94942aa44a7f83fb387e4
BtoBastian <bastianoppermann1997@gmail.com>
2017-10-22 11:19:21 +0200

    Fixed Javadoc comment

--- src/main/java/de/btobastian/javacord/utils/handler/server/GuildMemberUpdateHandler.java
+++ src/main/java/de/btobastian/javacord/utils/handler/server/GuildMemberUpdateHandler.java
@@ -31,7 +31,7 @@ import java.util.List;
 import java.util.Objects;
 
 /**
- * Handles the guild member add packet.
+ * Handles the guild member update packet.
  */
 public class GuildMemberUpdateHandler extends PacketHandler {
 
