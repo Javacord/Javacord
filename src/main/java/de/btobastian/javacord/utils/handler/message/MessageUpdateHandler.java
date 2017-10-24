a0c4062652d7511c8be4c6bbb66b64d4c90b42b9
BtoBastian <bastianoppermann1997@gmail.com>
2017-10-24 10:24:00 +0200

    Updated the MessageUpdateHandler

--- src/main/java/de/btobastian/javacord/utils/handler/message/MessageUpdateHandler.java
+++ src/main/java/de/btobastian/javacord/utils/handler/message/MessageUpdateHandler.java
@@ -55,7 +55,7 @@ public class MessageUpdateHandler extends PacketHandler {
             if (packet.has("edited_timestamp") && !packet.isNull("edited_timestamp")) {
                 message.ifPresent(msg ->
                         msg.setLastEditTime(OffsetDateTime.parse(packet.getString("edited_timestamp")).toInstant()));
-                
+
                 long editTimestamp =
                         OffsetDateTime.parse(packet.getString("edited_timestamp")).toInstant().toEpochMilli();
                 long lastKnownEditTimestamp = lastKnownEditTimestamps.getOrDefault(messageId, 0L);
