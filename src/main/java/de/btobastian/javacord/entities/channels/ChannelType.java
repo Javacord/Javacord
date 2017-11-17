cc715ba6eeb4891b662c836fb10a903ea8271cb7
BtoBastian <bastianoppermann1997@gmail.com>
2017-11-17 15:30:48 +0100

    Improved the ChannelType enum

--- src/main/java/de/btobastian/javacord/entities/channels/ChannelType.java
+++ src/main/java/de/btobastian/javacord/entities/channels/ChannelType.java
@@ -5,10 +5,49 @@ package de.btobastian.javacord.entities.channels;
  */
 public enum ChannelType {
 
-    CHANNEL_CATEGORY(),
-    GROUP_CHANNEL(),
-    PRIVATE_CHANNEL(),
-    SERVER_TEXT_CHANNEL(),
-    SERVER_VOICE_CHANNEL()
+    SERVER_TEXT_CHANNEL(0),
+    PRIVATE_CHANNEL(1),
+    SERVER_VOICE_CHANNEL(2),
+    GROUP_CHANNEL(3),
+    CHANNEL_CATEGORY(4),
+    UNKNOWN(-1);
+
+    /**
+     * The id of the channel type.
+     */
+    private final int id;
+
+    /**
+     * Creates a new channel type.
+     *
+     * @param id The id of the channel type.
+     */
+    ChannelType(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets the id of the channel type.
+     *
+     * @return The id of the channel type.
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Gets a channel type by it's id.
+     *
+     * @param id The id of the channel type.
+     * @return The channel type with the given id.
+     */
+    public static ChannelType fromId(int id) {
+        for (ChannelType type : values()) {
+            if (type.getId() == id) {
+                return type;
+            }
+        }
+        return UNKNOWN;
+    }
 
 }
