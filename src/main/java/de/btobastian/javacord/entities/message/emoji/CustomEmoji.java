729d15a727ef3d4dd5d1dc835c7d35e9a29b10f8
BtoBastian <bastianoppermann1997@gmail.com>
2017-12-24 15:27:33 +0100

    Added CustomEmoji#getImage()

--- src/main/java/de/btobastian/javacord/entities/message/emoji/CustomEmoji.java
+++ src/main/java/de/btobastian/javacord/entities/message/emoji/CustomEmoji.java
@@ -1,14 +1,25 @@
 package de.btobastian.javacord.entities.message.emoji;
 
 import de.btobastian.javacord.entities.DiscordEntity;
+import de.btobastian.javacord.entities.Icon;
 import de.btobastian.javacord.entities.Server;
+import de.btobastian.javacord.entities.impl.ImplIcon;
+import de.btobastian.javacord.utils.logging.LoggerUtil;
+import org.slf4j.Logger;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Optional;
 
 /**
  * This class represents a custom emoji.
  */
 public interface CustomEmoji extends DiscordEntity, Emoji {
+
+    /**
+     * The logger of this class.
+     */
+    Logger logger = LoggerUtil.getLogger(CustomEmoji.class);
 
     /**
      * Gets the name of the emoji.
@@ -24,6 +35,16 @@ public interface CustomEmoji extends DiscordEntity, Emoji {
      * @return The server of the emoji.
      */
     Optional<Server> getServer();
+
+    default Icon getImage() {
+        String urlString = "https://cdn.discordapp.com/emojis/" + getId() + (isAnimated() ? ".gif" : ".png");
+        try {
+            return new ImplIcon(getApi(), new URL(urlString));
+        } catch (MalformedURLException e) {
+            logger.warn("Seems like the url of the avatar is malformed! Please contact the developer!", e);
+            return null;
+        }
+    }
 
     @Override
     default String getMentionTag() {
