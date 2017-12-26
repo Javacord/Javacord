859c7003fac1428540f32955a24738d1ee2571a5
BtoBastian <bastianoppermann1997@gmail.com>
2017-12-26 13:06:40 +0100

    Added Server#canBanUsers(User)

--- src/main/java/de/btobastian/javacord/entities/Server.java
+++ src/main/java/de/btobastian/javacord/entities/Server.java
@@ -1012,6 +1012,17 @@ public interface Server extends DiscordEntity {
     }
 
     /**
+     * Checks if the given user can ban users from the server.
+     *
+     * @param user The user to check.
+     * @return Whether the given user can ban users or not.
+     */
+    default boolean canBanUsers(User user) {
+        return hasPermissions(user, PermissionType.ADMINISTRATOR) ||
+                hasPermissions(user, PermissionType.BAN_MEMBERS);
+    }
+
+    /**
      * Adds a listener, which listens to message creates in this server.
      *
      * @param listener The listener to add.
