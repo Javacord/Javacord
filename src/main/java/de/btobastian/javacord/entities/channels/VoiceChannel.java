de6cee78b698a538c40b57fc27575ca10627424e
BtoBastian <bastianoppermann1997@gmail.com>
2017-12-26 16:53:08 +0100

    Added VoiceChannel#canMuteUsers(User)

--- src/main/java/de/btobastian/javacord/entities/channels/VoiceChannel.java
+++ src/main/java/de/btobastian/javacord/entities/channels/VoiceChannel.java
@@ -28,4 +28,21 @@ public interface VoiceChannel extends Channel {
                 || severTextChannel.get().hasPermissions(user, PermissionType.VOICE_CONNECT);
     }
 
+    /**
+     * Checks if the given user can mute other users in this voice channel.
+     * In private chats (private channel or group channel) this always returns <code>false</code>.
+     *
+     * @param user The user to check.
+     * @return Whether the given user can mute other users or not.
+     */
+    default boolean canMuteUsers(User user) {
+        if (!canConnect(user) || getType() == ChannelType.PRIVATE_CHANNEL || getType() == ChannelType.GROUP_CHANNEL) {
+            return false;
+        }
+        Optional<ServerVoiceChannel> serverVoiceChannel = asServerVoiceChannel();
+        return !serverVoiceChannel.isPresent()
+                || serverVoiceChannel.get().hasPermissions(user, PermissionType.ADMINISTRATOR)
+                || serverVoiceChannel.get().hasPermissions(user, PermissionType.VOICE_MUTE_MEMBERS);
+    }
+
 }
