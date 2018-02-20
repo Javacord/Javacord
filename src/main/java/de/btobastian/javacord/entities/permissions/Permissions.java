c7214e4e827615ed8a796794ce15ef2bb5e0fbfd
BtoBastian <bastianoppermann1997@gmail.com>
2018-02-20 19:37:52 +0100

    Added Permissions#getAllowed/Denied/UnsetPermission()

--- src/main/java/de/btobastian/javacord/entities/permissions/Permissions.java
+++ src/main/java/de/btobastian/javacord/entities/permissions/Permissions.java
@@ -1,5 +1,9 @@
 package de.btobastian.javacord.entities.permissions;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+
 /**
  * A collection of all existing {@link PermissionType permission types} and their states.
  */
@@ -12,6 +16,51 @@ public interface Permissions {
      * @return The state of the type.
      */
     PermissionState getState(PermissionType type);
+
+    /**
+     * Gets a collection with permission types which are set to ({@link PermissionState#ALLOWED}).
+     *
+     * @return A collection with all allowed permissions.
+     */
+    default Collection<PermissionType> getAllowedPermission() {
+        Collection<PermissionType> types = new HashSet<>();
+        for (PermissionType type : PermissionType.values()) {
+            if (getState(type) == PermissionState.ALLOWED) {
+                types.add(type);
+            }
+        }
+        return Collections.unmodifiableCollection(types);
+    }
+
+    /**
+     * Gets a collection with permission types which are set to ({@link PermissionState#DENIED}).
+     *
+     * @return A collection with all denied permissions.
+     */
+    default Collection<PermissionType> getDeniedPermissions() {
+        Collection<PermissionType> types = new HashSet<>();
+        for (PermissionType type : PermissionType.values()) {
+            if (getState(type) == PermissionState.DENIED) {
+                types.add(type);
+            }
+        }
+        return Collections.unmodifiableCollection(types);
+    }
+
+    /**
+     * Gets a collection with permission types which are set to ({@link PermissionState#NONE}).
+     *
+     * @return A collection with all unset permissions.
+     */
+    default Collection<PermissionType> getUnsetPermissions() {
+        Collection<PermissionType> types = new HashSet<>();
+        for (PermissionType type : PermissionType.values()) {
+            if (getState(type) == PermissionState.NONE) {
+                types.add(type);
+            }
+        }
+        return Collections.unmodifiableCollection(types);
+    }
 
     /**
      * Checks if the all permission types are set to {@link PermissionState#NONE}.
