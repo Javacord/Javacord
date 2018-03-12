c7add0060607e47332b72bc80ad46361c1da47a1
Björn Kautler <Bjoern@Kautler.net>
2018-03-12 18:32:09 +0100

    Enable the fallback logger to show trace logging

--- src/main/java/org/javacord/util/logging/FallbackLoggerConfiguration.java
+++ src/main/java/org/javacord/util/logging/FallbackLoggerConfiguration.java
@@ -9,6 +9,8 @@ public class FallbackLoggerConfiguration {
 
     private static AtomicBoolean debug = new AtomicBoolean();
 
+    private static AtomicBoolean trace = new AtomicBoolean();
+
     private FallbackLoggerConfiguration() {
         throw new UnsupportedOperationException();
     }
@@ -24,11 +26,37 @@ public class FallbackLoggerConfiguration {
 
     /**
      * Sets whether debug logging should be enabled.
+     * Disabling debug logging automatically disables trace logging, too.
      *
      * @param debug Whether debug logging should be enabled or not.
      */
     public static void setDebug(boolean debug) {
         FallbackLoggerConfiguration.debug.set(debug);
+        if (!debug) {
+            trace.set(false);
+        }
+    }
+
+    /**
+     * Checks whether trace logging is enabled.
+     *
+     * @return Whether trace logging is enabled or not.
+     */
+    public static boolean isTraceEnabled() {
+        return trace.get();
+    }
+
+    /**
+     * Sets whether trace logging should be enabled.
+     * Enabling trace logging automatically enables debug logging, too.
+     *
+     * @param trace Whether trace logging should be enabled or not.
+     */
+    public static void setTrace(boolean trace) {
+        FallbackLoggerConfiguration.trace.set(trace);
+        if (trace) {
+            debug.set(true);
+        }
     }
 
 }
