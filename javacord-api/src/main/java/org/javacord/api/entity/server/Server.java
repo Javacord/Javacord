77883b0e0669a5cc091d8e8cc17d79f4f63000bf
Björn Kautler <Bjoern@Kautler.net>
2018-04-30 14:27:45 +0200

    Add Server#areYouMuted

--- javacord-api/src/main/java/org/javacord/api/entity/server/Server.java
+++ javacord-api/src/main/java/org/javacord/api/entity/server/Server.java
@@ -190,6 +190,15 @@ public interface Server extends DiscordEntity, UpdatableFromCache<Server> {
     }
 
     /**
+     * Gets your muted state.
+     *
+     * @return Whether you are muted.
+     */
+    default boolean areYouMuted() {
+        return isMuted(getApi().getYourself());
+    }
+
+    /**
      * Gets the muted state of the user with the given id.
      *
      * @param userId The id of the user to check.
