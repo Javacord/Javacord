3e56aa2b349b46eceb3c99cd6fcf9cabd042f3ab
Björn Kautler <Bjoern@Kautler.net>
2018-05-15 11:50:27 +0200

    Centralize the avatar URL construction logic

--- javacord-core/src/main/java/org/javacord/core/event/user/UserChangeAvatarEventImpl.java
+++ javacord-core/src/main/java/org/javacord/core/event/user/UserChangeAvatarEventImpl.java
@@ -3,22 +3,12 @@ package org.javacord.core.event.user;
 import org.javacord.api.entity.Icon;
 import org.javacord.api.entity.user.User;
 import org.javacord.api.event.user.UserChangeAvatarEvent;
-import org.javacord.core.entity.IconImpl;
-import org.javacord.core.util.logging.LoggerUtil;
-import org.slf4j.Logger;
-
-import java.net.MalformedURLException;
-import java.net.URL;
+import org.javacord.core.entity.user.UserImpl;
 
 /**
  * The implementation of {@link UserChangeAvatarEvent}.
  */
 public class UserChangeAvatarEventImpl extends UserEventImpl implements UserChangeAvatarEvent {
-
-    /**
-     * The logger of this class.
-     */
-    private static final Logger logger = LoggerUtil.getLogger(UserChangeAvatarEvent.class);
 
     /**
      * The new avatar hash of the user.
@@ -45,7 +35,7 @@ public class UserChangeAvatarEventImpl extends UserEventImpl implements UserChan
 
     @Override
     public Icon getNewAvatar() {
-        return getAvatar(newAvatarHash);
+        return UserImpl.getAvatar(api, newAvatarHash, getUser().getDiscriminator(), getUser().getId());
     }
 
     @Override
@@ -55,33 +45,12 @@ public class UserChangeAvatarEventImpl extends UserEventImpl implements UserChan
 
     @Override
     public Icon getOldAvatar() {
-        return getAvatar(oldAvatarHash);
+        return UserImpl.getAvatar(api, oldAvatarHash, getUser().getDiscriminator(), getUser().getId());
     }
 
     @Override
     public boolean oldAvatarIsDefaultAvatar() {
         return oldAvatarHash == null;
-    }
-
-    /**
-     * Gets the avatar by its hash.
-     *
-     * @param avatarHash The hash of the avatar.
-     * @return The icon with the given hash.
-     */
-    private Icon getAvatar(String avatarHash) {
-        String url = "https://cdn.discordapp.com/embed/avatars/"
-                + Integer.parseInt(getUser().getDiscriminator()) % 5 + ".png";
-        if (avatarHash != null) {
-            url = "https://cdn.discordapp.com/avatars/" + getUser().getIdAsString() + "/" + avatarHash
-                    + (avatarHash.startsWith("a_") ? ".gif" : ".png");
-        }
-        try {
-            return new IconImpl(getApi(), new URL(url));
-        } catch (MalformedURLException e) {
-            logger.warn("Seems like the url of the avatar is malformed! Please contact the developer!", e);
-            return null;
-        }
     }
 
 }
