c4c4d2ea6098c52d5aaefb6abb204af44c63196d
Björn Kautler <Bjoern@Kautler.net>
2018-10-26 02:08:29 +0200

    Test that WebSocket calls with a naive man-in-the-middle attack fail with SSLHandshakeException

--- javacord-core/src/test/groovy/org/javacord/core/util/gateway/DiscordWebSocketAdapterTest.groovy
+++ javacord-core/src/test/groovy/org/javacord/core/util/gateway/DiscordWebSocketAdapterTest.groovy
@@ -1,6 +1,7 @@
 package org.javacord.core.util.gateway
 
 import com.neovisionaries.ws.client.OpeningHandshakeException
+import com.neovisionaries.ws.client.WebSocketException
 import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.test.appender.ListAppender
 import org.javacord.core.DiscordApiImpl
@@ -14,6 +15,8 @@ import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Subject
 import spock.util.environment.RestoreSystemProperties
+
+import javax.net.ssl.SSLHandshakeException
 
 @Subject(DiscordWebSocketAdapter)
 class DiscordWebSocketAdapterTest extends Specification {
@@ -32,6 +35,61 @@ class DiscordWebSocketAdapterTest extends Specification {
 
     def cleanupSpec() {
         DiscordWebSocketAdapter.gateway = originalGateway
+    }
+
+    @RestoreSystemProperties
+    def 'WebSocket calls with a man-in-the-middle attack fail'() {
+        given:
+            MockProxyManager.mockProxy.when(
+                    HttpRequest.request()
+            ) respond HttpResponse.response().withStatusCode(HttpURLConnection.HTTP_NOT_FOUND)
+            MockProxyManager.setHttpSystemProperties()
+            DiscordApiImpl api = Stub {
+                getThreadPool() >> threadPool
+                // do not wait for identify rate limit by using a different token each time
+                getPrefixedToken() >> UUID.randomUUID().toString()
+            }
+
+        when:
+            new DiscordWebSocketAdapter(api, false)
+            ListAppender.getListAppender('Test Appender').events
+                    .findAll { it.level == Level.WARN }
+                    .findAll { it.thrown }
+                    .each { throw it.thrown }
+
+        then:
+            WebSocketException wse = thrown()
+            wse.cause instanceof SSLHandshakeException
+    }
+
+    @RestoreSystemProperties
+    def 'WebSocket calls with man-in-the-middle attack allowed do not fail with handshake exception'() {
+        given:
+            MockProxyManager.mockProxy.when(
+                    HttpRequest.request()
+            ) respond HttpResponse.response().withStatusCode(HttpURLConnection.HTTP_NOT_FOUND)
+            MockProxyManager.setHttpSystemProperties()
+            DiscordApiImpl api = Stub {
+                getThreadPool() >> threadPool
+                // do not wait for identify rate limit by using a different token each time
+                getPrefixedToken() >> UUID.randomUUID().toString()
+                isTrustAllCertificates() >> true
+            }
+
+        when:
+            new DiscordWebSocketAdapter(api, false)
+            ListAppender.getListAppender('Test Appender').events
+                    .findAll { it.level == Level.WARN }
+                    .findAll { it.thrown }
+                    .each { throw it.thrown }
+
+        then:
+            OpeningHandshakeException ohe = thrown()
+            ohe.message == 'The status code of the opening handshake response is not \'101 Switching Protocols\'. ' +
+                    'The status line is: HTTP/1.1 404 Not Found'
+
+        and:
+            MockProxyManager.mockProxy.verify HttpRequest.request(), VerificationTimes.atLeast(1)
     }
 
     @RestoreSystemProperties
