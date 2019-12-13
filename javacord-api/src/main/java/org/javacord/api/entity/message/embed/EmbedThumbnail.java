519d22d70e8ccb914bc284d4193ecfbd109b6cb7
robotman2412 <julian@scheffers.net>
2019-12-13 09:21:42 +0100

    Add download methods for embed thumbnails

--- javacord-api/src/main/java/org/javacord/api/entity/message/embed/EmbedThumbnail.java
+++ javacord-api/src/main/java/org/javacord/api/entity/message/embed/EmbedThumbnail.java
@@ -1,6 +1,12 @@
 package org.javacord.api.entity.message.embed;
 
+import org.javacord.api.DiscordApi;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * This interface represents an embed thumbnail.
@@ -34,5 +40,30 @@ public interface EmbedThumbnail {
      * @return The width of the thumbnail.
      */
     int getWidth();
+
+    /**
+     * Downloads the thumbnail as a {@code BufferedImage}.
+     * 
+     * @param api The discord api instance used to download the thumbnail.
+     * @return The thumbnail as a {@code BufferedImage}.
+     */
+    CompletableFuture<BufferedImage> downloadAsBufferedImage(DiscordApi api);
+
+    /**
+     * Downloads the thumbnail as a byte array.
+     *
+     * @param api The discord api instance used to download the thumbnail.
+     * @return The thumbnail as a byte array.
+     */
+    CompletableFuture<byte[]> downloadAsByteArray(DiscordApi api);
+
+    /**
+     * Downloads the thumbnail as an input stream.
+     *
+     * @param api The discord api instance used to download the thumbnail.
+     * @return The thumbnail as a input stream.
+     * @throws IOException If an IO error occurs.
+     */
+    InputStream downloadAsInputStream(DiscordApi api) throws IOException;
 
 }
