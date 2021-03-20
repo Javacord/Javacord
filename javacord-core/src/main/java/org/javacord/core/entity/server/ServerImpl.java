749451349816d1f22ea572ac552d40eb7ec203a3
Joshix <joshix.dev@mailbox.org>
2021-03-20 14:07:58 +0100

    allow banning users by id

--- javacord-core/src/main/java/org/javacord/core/entity/server/ServerImpl.java
+++ javacord-core/src/main/java/org/javacord/core/entity/server/ServerImpl.java
@@ -1364,9 +1364,9 @@ public class ServerImpl implements Server, Cleanupable, InternalServerAttachable
     }
 
     @Override
-    public CompletableFuture<Void> banUser(User user, int deleteMessageDays, String reason) {
+    public CompletableFuture<Void> banUser(String userId, int deleteMessageDays, String reason) {
         RestRequest<Void> request = new RestRequest<Void>(getApi(), RestMethod.PUT, RestEndpoint.BAN)
-                .setUrlParameters(getIdAsString(), user.getIdAsString())
+                .setUrlParameters(getIdAsString(), userId)
                 .addQueryParameter("delete_message_days", String.valueOf(deleteMessageDays));
         if (reason != null) {
             request.addQueryParameter("reason", reason);
