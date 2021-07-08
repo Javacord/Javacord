f6d245218daf1b9e8b47f7f0eaa62e092816d66f
Morazzer <moritzfiala1807@gmail.com>
2021-07-09 01:14:51 +0200

    Added missing server features

--- javacord-api/src/main/java/org/javacord/api/entity/server/ServerFeature.java
+++ javacord-api/src/main/java/org/javacord/api/entity/server/ServerFeature.java
@@ -6,37 +6,21 @@ package org.javacord.api.entity.server;
 public enum ServerFeature {
 
     /**
-     * Server has access to set an invite splash background.
+     * Server has access to set an animated Server icon.
      */
-    INVITE_SPLASH,
+    ANIMATED_ICON,
     /**
-     * Server has access to set 384kbps bitrate in voice (previously VIP voice servers).
+     * Server has access to set a Server banner image.
      */
-    VIP_REGIONS,
-    /**
-     * Server has access to set a vanity URL.
-     */
-    VANITY_URL,
-    /**
-     * Server is verified.
-     */
-    VERIFIED,
-    /**
-     * Server is partnered.
-     */
-    PARTNERED,
-    /**
-     * Server is a community server.
-     */
-    COMMUNITY,
+    BANNER,
     /**
      * Server has access to use commerce features (i.e. create store channels).
      */
     COMMERCE,
     /**
-     * Server has access to create news channels.
+     * Server is a community server.
      */
-    NEWS,
+    COMMUNITY,
     /**
      * Server is able to be discovered in the directory.
      */
@@ -46,23 +30,67 @@ public enum ServerFeature {
      */
     FEATURABLE,
     /**
-     * Server has access to set an animated Server icon.
+     * Server has access to set an invite splash background.
      */
-    ANIMATED_ICON,
-    /**
-     * Server has access to set a Server banner image.
-     */
-    BANNER,
-    /**
-     * Server has enabled the welcome screen.
-     */
-    WELCOME_SCREEN_ENABLED,
+    INVITE_SPLASH,
     /**
      * Server has enabled <a href="https://discord.com/developers/docs/resources/guild#membership-screening-object">Membership Screening</a>.
      */
     MEMBER_VERIFICATION_GATE_ENABLED,
     /**
+     * Server has access to create news channels.
+     */
+    NEWS,
+    /**
+     * Server is partnered.
+     */
+    PARTNERED,
+    /**
      * Server can be previewed before joining via Membership Screening or the directory.
      */
-    PREVIEW_ENABLED
+    PREVIEW_ENABLED,
+    /**
+     * Server has access to set a vanity URL.
+     */
+    VANITY_URL,
+    /**
+     * Server is verified.
+     */
+    VERIFIED,
+    /**
+     * Server has access to set 384kbps bitrate in voice (previously VIP voice servers).
+     */
+    VIP_REGIONS,
+    /**
+     * Server has enabled the welcome screen.
+     */
+    WELCOME_SCREEN_ENABLED,
+    /**
+     * Server has enabled ticketed events.
+     */
+    TICKETED_EVENTS_ENABLED,
+    /**
+     * Server has enabled monetization.
+     */
+    MONETIZATION_ENABLED,
+    /**
+     * Server has increased custom sticker slots.
+     */
+    MORE_STICKERS,
+    /**
+     * Server has access to the three day archive time for threads.
+     */
+    THREE_DAY_THREAD_ARCHIVE,
+    /**
+     * Server has access to the seven day archive time for threads.
+     */
+    SEVEN_DAY_THREAD_ARCHIVE,
+    /**
+     * Server has access to create private threads.
+     */
+    PRIVATE_THREADS,
+    /**
+     * Server was able to be discovered in the directory before.
+     */
+    ENABLED_DISCOVERABLE_BEFORE
 }
