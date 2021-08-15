2be42f1e646fd2e460563b9c080fca76c16810ed
Dominic Fellbaum <d.fellbaum@hotmail.de>
2021-08-15 15:53:10 +0200

    Add missing methods to get the actual component in HighLevelComponent and LowLevelComponent

--- javacord-api/src/main/java/org/javacord/api/entity/message/component/HighLevelComponent.java
+++ javacord-api/src/main/java/org/javacord/api/entity/message/component/HighLevelComponent.java
@@ -1,4 +1,26 @@
 package org.javacord.api.entity.message.component;
 
-public interface HighLevelComponent extends Component {
+import org.javacord.api.util.Specializable;
+import java.util.Optional;
+
+public interface HighLevelComponent extends Component, Specializable<HighLevelComponent> {
+
+    /**
+     * Whether this component is of this type.
+     *
+     * @return True if it's of that type.
+     */
+    default boolean isActionRow() {
+        return getType() == ComponentType.ACTION_ROW;
+    }
+
+    /**
+     * Gets the component as an ActionRow if it's of that type.
+     *
+     * @return The ActionRow.
+     */
+    default Optional<ActionRow> asActionRow() {
+        return isActionRow() ? Optional.of((ActionRow) this) : Optional.empty();
+    }
+
 }
--- javacord-api/src/main/java/org/javacord/api/entity/message/component/LowLevelComponent.java
+++ javacord-api/src/main/java/org/javacord/api/entity/message/component/LowLevelComponent.java
@@ -1,4 +1,44 @@
 package org.javacord.api.entity.message.component;
 
-public interface LowLevelComponent extends Component {
+import org.javacord.api.util.Specializable;
+import java.util.Optional;
+
+public interface LowLevelComponent extends Component, Specializable<LowLevelComponent> {
+
+    /**
+     * Whether this component is of this type.
+     *
+     * @return True if it's of that type.
+     */
+    default boolean isButton() {
+        return getType() == ComponentType.BUTTON;
+    }
+
+    /**
+     * Gets the component as a Button if it's of that type.
+     *
+     * @return The Button.
+     */
+    default Optional<Button> asButton() {
+        return isButton() ? Optional.of((Button) this) : Optional.empty();
+    }
+
+    /**
+     * Whether this component is of this type.
+     *
+     * @return True if it's of that type.
+     */
+    default boolean isSelectMenu() {
+        return getType() == ComponentType.SELECT_MENU;
+    }
+
+    /**
+     * Gets the component as a SelectMenu if it's of that type.
+     *
+     * @return The SelectMenu.
+     */
+    default Optional<SelectMenu> asSelectMenu() {
+        return isSelectMenu() ? Optional.of((SelectMenu) this) : Optional.empty();
+    }
+
 }
