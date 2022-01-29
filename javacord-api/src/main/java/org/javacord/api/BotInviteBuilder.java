3d9989da7c93f8fc333638c94e51ebb4ce87fb4c
ShindouMihou <kokocrunchsh@gmail.com>
2022-01-29 20:56:35 +0800

    Revamps the BotInviteBuilder to support InviteScopes, etc.

--- javacord-api/src/main/java/org/javacord/api/BotInviteBuilder.java
+++ javacord-api/src/main/java/org/javacord/api/BotInviteBuilder.java
@@ -1,7 +1,15 @@
 package org.javacord.api;
 
+import org.javacord.api.entity.permission.PermissionType;
 import org.javacord.api.entity.permission.Permissions;
 import org.javacord.api.entity.permission.PermissionsBuilder;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 /**
  * A tool to create bot invites.
@@ -9,20 +17,35 @@ import org.javacord.api.entity.permission.PermissionsBuilder;
 public class BotInviteBuilder {
 
     /**
-     * The base link of a bot invite.
-     */
-    public static final String BASE_LINK =
-            "https://" + Javacord.DISCORD_DOMAIN + "/oauth2/authorize?client_id=%s&scope=bot&permissions=%s";
-
-    /**
-     * The client id of the bot's application.
+     * The application client identification.
      */
     private long clientId;
+
+    /**
+     * The base link for a bot application invite link.
+     */
+    public static final String BASE_LINK =
+            "https://" + Javacord.DISCORD_DOMAIN + "/oauth2/authorize?client_id=";
+
+    /**
+     * The URI to redirect after inviting.
+     */
+    private String redirectUri = "";
+
+    /**
+     * The scopes to append to the link.
+     */
+    private Collection<BotInviteScope> inviteScopes = new ArrayList<>();
 
     /**
      * The permissions for the bot.
      */
     private Permissions permissions = new PermissionsBuilder().build();
+
+    /**
+     * To append the consent field into the invite builder or not.
+     */
+    private boolean consent = true;
 
     /**
      * Creates a new bot invite builder.
@@ -42,8 +65,17 @@ public class BotInviteBuilder {
         try {
             this.clientId = Long.parseLong(clientId);
         } catch (NumberFormatException e) {
-            this.clientId = 0;
+            this.clientId = 0L;
         }
+    }
+
+    /**
+     * Creates a new bot invite builder.
+     *
+     * @param api The Discord API to use.
+     */
+    public BotInviteBuilder(DiscordApi api) {
+        this.clientId = api.getClientId();
     }
 
     /**
@@ -58,12 +90,120 @@ public class BotInviteBuilder {
     }
 
     /**
+     * Sets the permissions the bot should have.
+     *
+     * @param permissions The permissions to set.
+     * @return The current instance in order to chain call methods.
+     */
+    public BotInviteBuilder setPermissions(PermissionType... permissions) {
+        this.permissions = new PermissionsBuilder()
+                .setAllowed(permissions)
+                .build();
+        return this;
+    }
+
+    /**
+     * Sets the redirect uri for this invite builder.
+     *
+     * @param redirectUri The redirect uri to set.
+     * @return The current instance in order to chain call methods.
+     */
+    public BotInviteBuilder setRedirectUri(String redirectUri) {
+        this.redirectUri = redirectUri;
+        return this;
+    }
+
+    /**
+     * Sets the scopes for this invite builder.
+     *
+     * @param scopes The scopes to set.
+     * @return The current instance in order to chain call methods.
+     */
+    public BotInviteBuilder setScopes(Collection<BotInviteScope> scopes) {
+        this.inviteScopes = scopes;
+        return this;
+    }
+
+    /**
+     * Sets the scopes for this invite builder.
+     *
+     * @param scopes The scopes to set.
+     * @return The current instance in order to chain call methods.
+     */
+    public BotInviteBuilder setScopes(BotInviteScope... scopes) {
+        this.inviteScopes = Arrays.asList(scopes);
+        return this;
+    }
+
+    /**
+     * Adds the scopes for this invite builder.
+     *
+     * @param scopes The scopes to add.
+     * @return The current instance in order to chain call methods.
+     */
+    public BotInviteBuilder addScopes(Collection<BotInviteScope> scopes) {
+        this.inviteScopes.addAll(scopes);
+        return this;
+    }
+
+    /**
+     * Adds the scopes for this invite builder.
+     *
+     * @param scopes The scopes to add.
+     * @return The current instance in order to chain call methods.
+     */
+    public BotInviteBuilder addScopes(BotInviteScope... scopes) {
+        this.inviteScopes.addAll(Arrays.asList(scopes));
+        return this;
+    }
+
+    /**
+     * Sets whether to append prompt consent to the invite link.
+     *
+     * @param promptConsent Whether to append prompt consent or not.
+     * @return The current instance in order to chain call methods.
+     */
+    public BotInviteBuilder setPromptConsent(boolean promptConsent) {
+        this.consent = promptConsent;
+        return this;
+    }
+
+    /**
      * Creates the invite link.
      *
      * @return The invite link for the bot.
      */
     public String build() {
-        return String.format(BASE_LINK, clientId, permissions.getAllowedBitmask());
+        if (inviteScopes.isEmpty()) {
+            addScopes(BotInviteScope.BOT, BotInviteScope.APPLICATIONS_COMMANDS);
+        }
+
+        StringBuilder builder = new StringBuilder(BASE_LINK);
+        builder.append(clientId)
+                .append("&scope=")
+                .append(
+                        inviteScopes.stream()
+                                .map(BotInviteScope::getScope)
+                                .collect(Collectors.joining("%20"))
+                )
+                .append("&permissions=")
+                .append(permissions.getAllowedBitmask());
+
+        if (redirectUri != null && !redirectUri.isEmpty()) {
+            try {
+                builder.append("&redirect_uri=")
+                        .append(URLEncoder.encode(redirectUri, "UTF-8"))
+                        .append("&response_type=code");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (consent) {
+            builder.append("&prompt=consent");
+        }
+
+        return builder.toString();
     }
 
 }
