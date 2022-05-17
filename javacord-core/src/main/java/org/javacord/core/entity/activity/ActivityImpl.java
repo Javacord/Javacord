5abb12f5e67843d78bd7c6728d1e6b5dd3ed3573
Yusuf <yusufgamer222@gmail.com>
2022-05-17 17:15:49 +0100

    Added missing fields to the Activity object.
    I have added the missing fields for the Activity Object.

--- javacord-core/src/main/java/org/javacord/core/entity/activity/ActivityImpl.java
+++ javacord-core/src/main/java/org/javacord/core/entity/activity/ActivityImpl.java
@@ -3,15 +3,19 @@ package org.javacord.core.entity.activity;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.javacord.api.entity.activity.Activity;
 import org.javacord.api.entity.activity.ActivityAssets;
+import org.javacord.api.entity.activity.ActivityButtons;
 import org.javacord.api.entity.activity.ActivityFlag;
 import org.javacord.api.entity.activity.ActivityParty;
+import org.javacord.api.entity.activity.ActivitySecrets;
 import org.javacord.api.entity.activity.ActivityType;
 import org.javacord.api.entity.emoji.Emoji;
 import org.javacord.core.DiscordApiImpl;
 import org.javacord.core.entity.emoji.UnicodeEmojiImpl;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -23,15 +27,19 @@ public class ActivityImpl implements Activity {
     private final ActivityType type;
     private final String name;
     private final String streamingUrl;
+    private final Instant createdAt;
     private final String details;
     private final String state;
     private final ActivityParty party;
     private final ActivityAssets assets;
+    private final ActivitySecrets secrets;
     private final Long applicationId;
     private final Long startTime;
     private final Long endTime;
     private final Emoji emoji;
+    private final Boolean instance;
     private final EnumSet<ActivityFlag> flags = EnumSet.noneOf(ActivityFlag.class);
+    private final List<ActivityButtons> buttons = new ArrayList<>();
 
     /**
      * Creates a new activity object.
@@ -47,7 +55,10 @@ public class ActivityImpl implements Activity {
         this.state = data.has("state") ? data.get("state").asText(null) : null;
         this.party = data.has("party") ? new ActivityPartyImpl(data.get("party")) : null;
         this.assets = data.has("assets") ? new ActivityAssetsImpl(this, data.get("assets")) : null;
+        this.secrets = data.has("secrets") ? new ActivitySecretsImpl(data.get("secrets")) : null;
         this.applicationId = data.has("application_id") ? data.get("application_id").asLong() : null;
+        this.createdAt = Instant.ofEpochMilli(data.get("created_at").asLong());
+        this.instance = data.has("instance") && data.get("instance").asBoolean();
 
         if (data.has("timestamps")) {
             JsonNode timestamps = data.get("timestamps");
@@ -77,6 +88,13 @@ public class ActivityImpl implements Activity {
                 }
             }
         }
+
+        if (data.has("buttons")) {
+            for (JsonNode button : data.get("buttons")) {
+                ActivityButtonsImpl activityButtons = new ActivityButtonsImpl(button);
+                this.buttons.add(activityButtons);
+            }
+        }
     }
 
     /**
@@ -90,14 +108,17 @@ public class ActivityImpl implements Activity {
         this.type = type;
         this.name = name;
         this.streamingUrl = streamingUrl;
+        this.createdAt = null;
         this.details = null;
         this.state = null;
         this.party = null;
         this.assets = null;
+        this.secrets = null;
         this.applicationId = null;
         this.startTime = null;
         this.endTime = null;
         this.emoji = null;
+        this.instance = null;
     }
 
     @Override
@@ -108,6 +129,11 @@ public class ActivityImpl implements Activity {
     @Override
     public Optional<String> getStreamingUrl() {
         return Optional.ofNullable(streamingUrl);
+    }
+
+    @Override
+    public Instant getCreatedAt() {
+        return createdAt;
     }
 
     @Override
@@ -136,6 +162,11 @@ public class ActivityImpl implements Activity {
     }
 
     @Override
+    public Optional<ActivitySecrets> getSecrets() {
+        return Optional.ofNullable(secrets);
+    }
+
+    @Override
     public Optional<Long> getApplicationId() {
         return Optional.ofNullable(applicationId);
     }
@@ -156,8 +187,18 @@ public class ActivityImpl implements Activity {
     }
 
     @Override
+    public Optional<Boolean> getInstance() {
+        return Optional.ofNullable(instance);
+    }
+
+    @Override
     public EnumSet<ActivityFlag> getFlags() {
         return EnumSet.copyOf(flags);
+    }
+
+    @Override
+    public List<ActivityButtons> getButtons() {
+        return buttons;
     }
 
     @Override
@@ -177,7 +218,10 @@ public class ActivityImpl implements Activity {
                 && Objects.deepEquals(startTime, otherActivity.startTime)
                 && Objects.deepEquals(endTime, otherActivity.endTime)
                 && Objects.deepEquals(emoji, otherActivity.emoji)
-                && Objects.deepEquals(flags, otherActivity.flags);
+                && Objects.deepEquals(flags, otherActivity.flags)
+                && Objects.deepEquals(buttons, otherActivity.buttons)
+                && Objects.deepEquals(secrets, otherActivity.secrets)
+                && Objects.deepEquals(instance, otherActivity.instance);
     }
 
     @Override
@@ -195,6 +239,10 @@ public class ActivityImpl implements Activity {
         int endTimeHash = endTime == null ? 0 : endTime.toString().hashCode();
         int emojiHash = emoji == null ? 0 : emoji.hashCode();
         int flagsHash = flags == null ? 0 : flags.hashCode();
+        int createdAtHash = createdAt == null ? 0 : createdAt.hashCode();
+        int secretsHash = secrets == null ? 0 : secrets.hashCode();
+        int instanceHash = instance == null ? 0 : instance.hashCode();
+        int buttonsHash = buttons == null ? 0 : buttons.hashCode();
 
         hash = hash * 11 + typeHash;
         hash = hash * 13 + nameHash;
@@ -208,6 +256,10 @@ public class ActivityImpl implements Activity {
         hash = hash * 43 + endTimeHash;
         hash = hash * 47 + emojiHash;
         hash = hash * 53 + flagsHash;
+        hash = hash * 59 + createdAtHash;
+        hash = hash * 61 + secretsHash;
+        hash = hash * 67 + instanceHash;
+        hash = hash * 71 + buttonsHash;
         return hash;
     }
 }
