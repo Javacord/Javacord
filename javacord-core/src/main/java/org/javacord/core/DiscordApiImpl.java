a433bad3efffa7c8c7c382fbaee33d8d5124c53e
Dominic Fellbaum <d.fellbaum@hotmail.de>
2022-10-09 18:48:10 +0200

    Remove okhttp shutdown calls when disconnecting the api because it uses daemon threads since 4.5.1

--- javacord-core/src/main/java/org/javacord/core/DiscordApiImpl.java
+++ javacord-core/src/main/java/org/javacord/core/DiscordApiImpl.java
@@ -1895,8 +1895,6 @@ public class DiscordApiImpl implements DiscordApi, DispatchQueueSelector {
                     disconnectFuture.get().complete(null);
                 }, 1, TimeUnit.MINUTES);
             }
-            httpClient.dispatcher().executorService().shutdown();
-            httpClient.connectionPool().evictAll();
         }
         return disconnectFuture.get();
     }
