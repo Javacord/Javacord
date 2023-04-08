4ddbe69db11cd7b2c16b8da3834a3278999fd0b9
Dominic Fellbaum <d.fellbaum@hotmail.de>
2023-04-08 16:39:34 +0200

    Remove and add missing ServerFeatures

--- javacord-api/src/main/java/org/javacord/api/entity/server/ServerFeature.java
+++ javacord-api/src/main/java/org/javacord/api/entity/server/ServerFeature.java
@@ -6,9 +6,21 @@ package org.javacord.api.entity.server;
 public enum ServerFeature {
 
     /**
+     * Server has access to set an animated server banner image.
+     */
+    ANIMATED_BANNER,
+    /**
      * Server has access to set an animated Server icon.
      */
     ANIMATED_ICON,
+    /**
+     * Server is using the old permissions configuration behavior.
+     */
+    APPLICATION_COMMAND_PERMISSIONS_V2,
+    /**
+     * Server has set up auto moderation rules.
+     */
+    AUTO_MODERATION,
     /**
      * Server has access to set a Server banner image.
      */
@@ -18,6 +30,18 @@ public enum ServerFeature {
      */
     COMMUNITY,
     /**
+     * Server has enabled monetization.
+     */
+    CREATOR_MONETIZABLE_PROVISIONAL,
+    /**
+     * Server has enabled the role subscription promo page.
+     */
+    CREATOR_STORE_PAGE,
+    /**
+     * Server has been set as a support server on the App Directory.
+     */
+    DEVELOPER_SUPPORT_SERVER,
+    /**
      * Server is able to be discovered in the directory.
      */
     DISCOVERABLE,
@@ -26,13 +50,21 @@ public enum ServerFeature {
      */
     FEATURABLE,
     /**
+     * Server has paused invites, preventing new users from joining.
+     */
+    INVITES_DISABLED,
+    /**
      * Server has access to set an invite splash background.
      */
     INVITE_SPLASH,
     /**
-     * Server has enabled <a href="https://discord.com/developers/docs/resources/guild#membership-screening-object">Membership Screening</a>.
+     * Server has enabled <a href="https://discord.com/developers/docs/resources/#membership-screening-object">Membership Screening</a>.
      */
     MEMBER_VERIFICATION_GATE_ENABLED,
+    /**
+     * Server has increased custom sticker slots.
+     */
+    MORE_STICKERS,
     /**
      * Server has access to create news channels.
      */
@@ -45,6 +77,22 @@ public enum ServerFeature {
      * Server can be previewed before joining via Membership Screening or the directory.
      */
     PREVIEW_ENABLED,
+    /**
+     * Server is able to set role icons.
+     */
+    ROLE_ICONS,
+    /**
+     * Server has role subscriptions that can be purchased.
+     */
+    ROLE_SUBSCRIPTIONS_AVAILABLE_FOR_PURCHASE,
+    /**
+     * Server has enabled role subscriptions.
+     */
+    ROLE_SUBSCRIPTIONS_ENABLED,
+    /**
+     * Server has enabled ticketed events.
+     */
+    TICKETED_EVENTS_ENABLED,
     /**
      * Server has access to set a vanity URL.
      */
@@ -61,33 +109,4 @@ public enum ServerFeature {
      * Server has enabled the welcome screen.
      */
     WELCOME_SCREEN_ENABLED,
-    /**
-     * Server has enabled ticketed events.
-     */
-    TICKETED_EVENTS_ENABLED,
-    /**
-     * Server has enabled monetization.
-     */
-    MONETIZATION_ENABLED,
-    /**
-     * Server has increased custom sticker slots.
-     */
-    MORE_STICKERS,
-    /**
-     * Server has access to create private threads.
-     */
-    PRIVATE_THREADS,
-    /**
-     * Server has access to set an animated guild banner image.
-     */
-    ANIMATED_BANNER,
-    /**
-     * Server has set up auto moderation rules.
-     */
-    AUTO_MODERATION,
-
-    /**
-     * Server has paused invites, preventing new users from joining.
-     */
-    INVITES_DISABLED,
 }
