010d05f67516cbcdaf17bea3b3417da8df758dbb
Dominic Fellbaum <d.fellbaum@hotmail.de>
2023-05-06 20:23:54 +0200

    Add getters to get the value of an activity asset image in case of other services like Spotify, YouTube etc. as they have other CDNs

--- javacord-api/src/main/java/org/javacord/api/entity/activity/ActivityAssets.java
+++ javacord-api/src/main/java/org/javacord/api/entity/activity/ActivityAssets.java
@@ -1,7 +1,6 @@
 package org.javacord.api.entity.activity;
 
 import org.javacord.api.entity.Icon;
-
 import java.util.Optional;
 
 /**
@@ -10,7 +9,23 @@ import java.util.Optional;
 public interface ActivityAssets {
 
     /**
+     * Gets the large image value of the asset.
+     * If the value is from a different platform like Spotify, YouTube, etc. you should try to get the image from
+     * one of their CDNs which can be:
+     * <ul>
+     *     <li>Spotify: {@code https://i.scdn.co/image/{cover_id}}</li>
+     *     <li>Youtube: {@code https://i.ytimg.com/vi/{video_id}/hqdefault_live.jpg}</li>
+     *     <li>Twitch: {@code https://static-cdn.jtvnw.net/previews-ttv/live_user_{user_id}.png}</li>
+     * </ul>
+     * but take these with a grain of salt as they can change at any time.
+     *
+     * @return The large image value of the asset.
+     */
+    Optional<String> getLargeImageValue();
+
+    /**
      * Gets the large image of the asset.
+     * Always empty for activities of other services like Spotify, YouTube, etc.
      *
      * @return The large image of the asset.
      */
@@ -24,7 +39,23 @@ public interface ActivityAssets {
     Optional<String> getLargeText();
 
     /**
+     * Gets the small image value of the asset.
+     * If the value is from a different platform like Spotify, Youtube, etc. you should try to get the image from
+     * one of their CDNs which can be:
+     * <ul>
+     *     <li>Spotify: {@code https://i.scdn.co/image/{cover_id}}</li>
+     *     <li>Youtube: {@code https://i.ytimg.com/vi/{video_id}/hqdefault_live.jpg}</li>
+     *     <li>Twitch: {@code https://static-cdn.jtvnw.net/previews-ttv/live_user_{user_id}.png}</li>
+     * </ul>
+     * but take these with a grain of salt as they can change at any time.
+     *
+     * @return The small image value of the asset.
+     */
+    Optional<String> getSmallImageValue();
+
+    /**
      * Gets the small image of the asset.
+     * Always empty for activities of other services like Spotify, YouTube, etc.
      *
      * @return The small image of the asset.
      */
